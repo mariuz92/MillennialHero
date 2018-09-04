0e18d12c6762ef1abcaa745e91f8ca55375feb22
mariuz92 <mariotilita@gmail.com>
2018-09-05 00:26:11 +0200

    development

--- php_mail/contact.php
+++ php_mail/contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = "hello@themillennialhero.com";
+$from = "hello@millennialhero.com";
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'mariotilita@gmail.com';
